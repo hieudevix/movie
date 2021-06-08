fa8de34e5db9e6f1d9a635d95abaaac84c6d0fb8
hieu091099 <vai22806@gmail.com>
2021-06-08 16:18:14 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './css/main.css';
 import Loader from './components/Loader/Loader';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Loader/>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,20 @@ export default class Header extends Component {
     render() {
         return (
             <div>
-                
+                <nav className="header">
+                    <div className="header__logo">
+                        <img src="./images/logo.png"/>
+                    </div>
+                    <div className="header__nav">
+                        <ul>
+                            <li>Lịch Chiếu</li>
+                            <li>Cụm Rạp</li>
+                            <li>Tin Tức</li>
+                            <li>Ứng Dụng</li>
+                        </ul>
+                    </div>
+                    <div className="header__detail"></div>
+                </nav>
             </div>
         )
     }
