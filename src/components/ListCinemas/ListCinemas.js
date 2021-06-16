2fd4bab13afef71501375e23bd0e5407c9b0796e
hieu091099 <vai22806@gmail.com>
2021-06-16 21:29:12 +0700

    hello

--- src/components/ListCinemas/ListCinemas.js
+++ src/components/ListCinemas/ListCinemas.js
@@ -2,31 +2,29 @@ import React, { useEffect, useState } from 'react'
 import { Tabs, Radio, Space } from 'antd';
 import { Row, Col } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
-import { getListCinemaDetail, getListCinemas } from '../../redux/actions/ListCinemaAction';
+import { getListCinemaDetail } from '../../redux/actions/ListCinemaAction';
 const { TabPane } = Tabs;
 export default function ListCinemas() {
     // const [state, setState] = useState({
     //     tabPosition: 'left'
     // })
-    const { cinemaCodeList, listCinemas, listCinemasDetail } = useSelector(state => state.ListCinemasReducer);
+    const { cinemaCodeList, listCinemasDetail } = useSelector(state => state.ListCinemasReducer);
     // console.log('listCinema', cinemaCodeList[0].cinemaCode);
-    console.log('listCinemas', listCinemas);
     console.log('listCinemasDetail', listCinemasDetail);
     const dispatch = useDispatch();
     useEffect(() => {
         for (let i = 0; i < cinemaCodeList.length; i++) {
-            dispatch(getListCinemas(cinemaCodeList[i].cinemaCode));
             dispatch(getListCinemaDetail(cinemaCodeList[i].cinemaCode));
         }
     }, [])
     const renderListCinema = () => {
-        return listCinemas?.map((l, index) => {
-            return l.map((c, indexs) => {
-                return <TabPane tab={<img src={c.logo} width={50} height={50}/>}  key={index}>
-                    {listCinemasDetail.mapp((ld, indexss)=>{
-                        return 
+        return listCinemasDetail.map((listCinema, index) => {
+            return listCinema.map((lc,indexs)=>{
+                return <TabPane tab={<img src={lc.logo} width={50} height={50} />} key={index}>
+                    {lc.lstCumRap.map((lcd, index)=>{
+                        return lcd.tenCumRap;
                     })}
-                </TabPane>
+            </TabPane>
             })
         })
 
--- src/redux/actions/ListCinemaAction.js
+++ src/redux/actions/ListCinemaAction.js
@@ -1,27 +1,27 @@
 import axios from 'axios';
 
-export const getListCinemas = (maHeThongRap) =>{
+// export const getListCinemas = (maHeThongRap) =>{
 
-    return async (dispatch) =>{
-        try{
-            let result = await axios({
-                url:`https://movie0706.cybersoft.edu.vn/api/QuanLyRap/LayThongTinHeThongRap?maHeThongRap=${maHeThongRap}`,
-                method:'GET'
-            });
-            // console.log(result.data);
-            dispatch({type:'GET_LIST_CINEMAS', listCinemas: result.data})
-        } catch(errors){
-            console.log(errors);
-        }
-    }
-}
+//     return async (dispatch) =>{
+//         try{
+//             let result = await axios({
+//                 url:`https://movie0706.cybersoft.edu.vn/api/QuanLyRap/LayThongTinHeThongRap?maHeThongRap=${maHeThongRap}`,
+//                 method:'GET'
+//             });
+//             // console.log(result.data);
+//             dispatch({type:'GET_LIST_CINEMAS', listCinemas: result.data})
+//         } catch(errors){
+//             console.log(errors);
+//         }
+//     }
+// }
 
 
 export const getListCinemaDetail = (maHeThongRap) =>{
     return async (dispatch) =>{
         try{
             let result = await axios({
-                url:`https://movie0706.cybersoft.edu.vn/api/QuanLyRap/LayThongTinCumRapTheoHeThong?maHeThongRap=${maHeThongRap}`,
+                url:`https://movie0706.cybersoft.edu.vn/api/QuanLyRap/LayThongTinLichChieuHeThongRap?maHeThongRap=${maHeThongRap}&maNhom=GP03`,
                 method:'GET'
             });
             // console.log(result.data);
